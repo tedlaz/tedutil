a9d1e2795f96309157211115f29f3530885b39fe
Ted Lazaros <tedlaz@gmail.com>
2019-04-15 20:17:12 +0300

    calculator cleaned

--- tedutil/calculator.py
+++ tedutil/calculator.py
@@ -1,164 +1,147 @@
-"""Spreadsheet like functionality"""
-
-
 from collections import defaultdict
 from collections import namedtuple
 from functools import lru_cache
 import os
 from decimal import Decimal
-from functools import reduce
+# from functools import reduce
 from tedutil.dec import dec
-from tedutil.dec import is_number as isn
+from tedutil.dec import is_number
+from tedutil.dec import dec_with_given_digits
 # from tedutil.decorators import memoize
-# from .utils import calcforos
-# from .utils import calceea
+from tedutil.taxes import foros_periodoy
+from tedutil.taxes import eea_periodoy
 
-Alg = namedtuple('Alg', "operator decimals result parameters")
+
+def calcdispatch(fn):
+    # registry = {'ERROR': fn}
+    registry = dict()
+
+    def register(symbol):
+        def inner(fn):
+            registry[symbol.upper()] = fn
+            return fn
+
+        return inner
+
+    def decorator(*arg):
+        if arg[0].upper() not in registry:
+            raise ValueError(
+                "Symbol %s not in registry (args: %s)" % (arg[0], arg))
+        fun = registry[arg[0].upper()]
+        return fun(*arg[1:])
+
+    def registered():
+        return tuple(registry.keys())
+
+    decorator.register = register
+    decorator.registered = registered
+    return decorator
+
+
+Alg = namedtuple('Alg', "operator decimals result pars")
 
 
 @lru_cache()
-def load_algorithm(algorithm, default_decimals=6):
+def load_algorithm(algorithm):
     algorithm_lines = []
-    allowed_operators = tuple("=%*/+->^?dDfF")
+    # allowed = tuple("=%*/+->^?dDfF")
     if os.path.isfile(algorithm):
         with open(algorithm, encoding="utf8") as fil:
             adata = fil.read()
     else:
         adata = algorithm
     lines = adata.split('\n')
-    if len(lines) == 1:
-        raise ValueError("%s is neither existing file or valid string" % adata)
     for lin in lines:
-        if len(lin) < 3:
+        if len(lin) < 3 or lin.startswith('#') or lin.startswith(' '):
             continue
-        elif lin.startswith('#'):
-            continue
-        elif lin.startswith(allowed_operators):
-            (operator, *ldecimals), value, *params = lin.upper().split()
-            if len(ldecimals) == 0:
-                decimals = default_decimals
-            elif len(ldecimals) == 1:
-                decimals = ldecimals[0]
-                if not decimals.isdigit():
-                    raise ValueError("decimal value must be digit %s" % lin)
-            else:
-                raise ValueError("Wrong number of decimals in %s " % (lin))
-
-            algorithm_lines.append(Alg(operator, decimals, value, params))
-        else:
-            raise ValueError("Wrong operator %s in line %s" % (lin[0], lin))
+        operator, decimals, value, *params = lin.upper().split()
+        algorithm_lines.append(Alg(operator, decimals, value, params))
     return algorithm_lines
 
 
-
-def calc2(algorithm, data, function_dict=None, decimals=6):
-    alg_lines = load_algorithm(algorithm)
-    dvl = defaultdict(Decimal)
+def calculate(alg_lines, data):
+    res = defaultdict(Decimal)
     for key, val in data.items():
-        dvl[key.upper()] = Decimal(val)
-    for lin in alg_lines:
-        dc = lin.decimals
-        res = lin.result
-        par = lin.parameters
-        if lin.operator == '=':
-            dvl[res] = dec(par[0] if isn(par[0]) else dvl[par[0]], dc)
-        elif lin.operator == '%':
-            pr1 = dec(par[0] if isn(par[0]) else dvl[par[0]], dc)
-            dvl[res] = dec(pr1 / dec(100), dc)
-        elif lin.operator == '*':
-            pr1 = [Decimal(i) if isn(i) else dvl[i] for i in par]
-            dvl[res] = dec(reduce((lambda x, y: x * y), pr1), dc)
-        elif lin.operator == '/':
-            pr1 = [Decimal(i) if isn(i) else dvl[i] for i in par]
-            dvl[res] = dec(pr1[0] / pr1[1], dc)
-        elif lin.operator == '+':
-            pr1 = [Decimal(i) if isn(i) else dvl[i] for i in par]
-            dvl[res] = dec(sum(pr1), dc)
-        elif lin.operator == '-':
-            pr1 = [Decimal(i) if isn(i) else dvl[i] for i in par]
-            pr2 = [i for i in pr1[1:]]  # αντιστρέφω το πρόσημο
-            dvl[res] = dec(pr1[0] - sum(pr2), dc)
-        elif lin.operator == '>':
-            pr1 = [Decimal(i) if isn(i) else dvl[i] for i in par]
-            dif = dec(pr1[0] - pr1[1], dc)
-            dvl[res] = dif if dif > 0 else dec(0)
-        elif lin.operator == '^':
-            pr1 = [Decimal(i) if isn(i) else dvl[i] for i in par]
-            dif = dec(abs(pr1[0] - pr1[1]), dc)
-            dvl[res] = dif if dif > pr1[2] else dec(0)
-        elif lin.operator.upper() == 'D':
-            dvl[res] = dec(dvl[res], dc)
-        elif lin.operator == '?':
-            par.append(res)
-            spar = set(par)
-            sinp = set([i.upper() for i in data.keys()])
-            if not spar.issubset(sinp):
-                raise ValueError('Απαιτουνται %s και δόθηκαν %s' % (spar, sinp))
-        elif lin.operator.upper() == 'F':
-            strfunct = par[0]
-            pr1 = [Decimal(i) if isn(i) else dvl[i] for i in par[1:]]
-            funct = function_dict[strfunct]
-            dvl[res] = funct(*pr1)
-        else:
-            raise ValueError('Error in line : %s' % lin.operator)
-    return dvl
+        res[key.upper()] = dec_with_given_digits(val)
+    for el in alg_lines:
+        # args = [dec(i) if is_number(i) else res.get(i, dec(0)) for i in el.pars]
+        args = [dec(i) if is_number(i) else res[i] for i in el.pars]
+        res[el.result] = dec(calc(el.operator, args), el.decimals)
+    return res
 
 
-def calculator(algorithm, data, function_dict=None, des=6):
-    alg_lines = algorithm.split('\n')
-    vrs = defaultdict(Decimal)
-    for key, value in data.items():
-        vrs[key] = dec(value, des)
-    for lin in alg_lines:
-        if len(lin.strip()) < 3:
-            continue
-        if lin.startswith('#'):
-            continue
-        operator, key, *dat = lin.split()
-        psize = len(dat)
-        par = []
-        # Εδώ δημιουργούμε δεκαδικές τιμές είτε καλούμε μεταβλητές που υπάρχουν
-        for el in dat:
-            par.append(dec(el, des) if isn(el) else vrs[el])
-        # Απο εδώ ξεκινάει ο υπολογισμός
-        if len(operator) == 2:
-            decim = int(operator[1])
-            operator = operator[0]
-        else:
-            decim = des
-        # print(operator, decim)
-        if operator == '=' and psize == 1:  # Iσότητα δύο μεταβλητές
-            vrs[key] = dec(par[0], decim)
-        elif operator == '%' and psize == 1:
-            vrs[key] = dec(par[0] / dec(100), decim)
-        elif operator == '*':
-            vrs[key] = dec(reduce((lambda x, y: x * y), par), decim)
-        elif operator == '/' and psize == 2:
-            vrs[key] = dec(par[0] / par[1], decim)
-        elif operator == '+':
-            vrs[key] = dec(sum(par), decim)
-        elif operator == '-' and psize == 2:
-            vrs[key] = dec(par[0] - par[1], decim)
-        elif operator == '>' and psize == 2:
-            dif = dec(par[0] - par[1], decim)
-            vrs[key] = dif if dif > 0 else dec(0)
-        elif operator == '^' and psize == 3:
-            dif = dec(abs(par[0] - par[1]), decim)
-            vrs[key] = dif if dif > par[2] else dec(0)
-        elif operator == 'd' and psize == 0:  # Στρογγυλοποίηση σε δεκαδικά
-            vrs[key] = dec(vrs[key], decim)
-        elif operator == '?':
-            # Απαιτούμενες παράμετροι εισόδου
-            _, *sdata = lin.split()
-            spar = set(sdata)
-            sinp = set(data.keys())
-            if not spar.issubset(sinp):
-                raise ValueError('Απαιτουνται %s και δόθηκαν %s' % (spar, sinp))
-        elif operator == 'f':  #  Εδώ μπαίνουν συναρτήσεις
-            _, _, strfunct, *_ = lin.split()
-            funct = function_dict[strfunct]
-            vrs[key] = funct(*par[1:])
-            del vrs[strfunct]  # Διαγράφουμε από το dictionary τη συνάρτηση
-        else:
-            raise ValueError('Error in line : %s' % lin)
-    return vrs
+@calcdispatch
+def calc():
+    return 0
+
+
+@calc.register('=')
+def _(arg):
+    return arg[0]
+
+
+@calc.register('%')
+def _(arg):
+    return arg[0] / dec(100)
+
+
+@calc.register('/')
+def _(arg):
+    return arg[0] / arg[1]
+
+
+@calc.register('*')
+def _(arg):
+    mult = 1
+    for el in arg:
+        mult = mult * el
+    return mult
+
+
+@calc.register('+')
+def _(vlist):
+    return sum(vlist)
+
+
+@calc.register('-')
+def _(arg):
+    return arg[0] - sum(arg[1:])
+
+
+@calc.register('>')
+def _(arglist):
+    dif = arglist[0] - arglist[1]
+    return dif if dif > 0 else 0
+
+
+@calc.register('^')
+def _(alist):
+    dif = abs(alist[0] - alist[1])
+    return dif if dif >= alist[2] else 0
+
+
+@calc.register('foros')
+def _(alist):
+    #2019 FOROLOGITEO PAIDIA BARYTIS-PERIODOY
+    etos, forologiteo, paidia, barytis, *_ = alist
+    return foros_periodoy(etos, forologiteo, paidia, barytis)
+
+
+@calc.register('eea')
+def _(alist):
+    #2019 FOROLOGITEO PAIDIA BARYTIS-PERIODOY
+    etos, forologiteo, barytis, *_ = alist
+    return eea_periodoy(etos, forologiteo, barytis)
+
+
+if __name__ == "__main__":
+    alg = load_algorithm('/home/ted/alg2.txt')
+    dat = {'IMEROMISTHIO': 60,
+           'meres': 25,
+           'POSOSTO-IKA': 44.15,
+           'POSOSTO-IKA-ERGAZOMENOY': 14.98,
+           'paidia': 3}
+    # print('\n'.join([f'{i.operator} {i.result} {i.pars}' for i in alg]))
+    print('\n'.join('%-30s: %9s' % (i, j)
+                    for i, j in calculate(alg, dat).items()))
+    print(calc.registered())
--- tedutil/tests/test_calculator.py
+++ tedutil/tests/test_calculator.py
@@ -1,98 +1,16 @@
-from unittest import TestCase
-from decimal import Decimal
-from tedutil import calculator as clc
-from tedutil.taxes import foros_periodoy
-from tedutil.taxes import eea_periodoy
-fundic = {'CALC-FOROS': foros_periodoy, 'CALC-EEA': eea_periodoy}
 
 
-mis = '''
-#  This is a test
-?  imeromisthio meres pikatotal pikaenos
-=0 BARPERIODOY  14
-%4 PIKAT       pikatotal
-%4 PIKAE       pikaenos
-%4 PNYXTA       25
-%4 PARGIA       75
-%2 SYNTFOROY    22
-/6 POROM        6 40
-/4 ORESANAMERA  40 6
-*2 OROMISTHIO   imeromisthio POROM 
-*2 NYXTPROS     oresnyxta OROMISTHIO PNYXTA
-*2 ARGIAPROS    meresargia imeromisthio PARGIA 
-*2 NAPOD        imeromisthio meres
-+2 TAPODOXES    NAPOD NYXTPROS ARGIAPROS
-*2 IKATOTAL     TAPODOXES PIKAT
-*2 IKAERGNO     TAPODOXES PIKAE
--2 IKAERDOTIS   IKATOTAL IKAERGNO
--2 FOROLOGITEO  TAPODOXES IKAERGNO
-f  FOROSPERIODO CALC-FOROS 2019 FOROLOGITEO PAIDIA BARPERIODOY
-f  EEAPERIODOY  CALC-EEA 2019 FOROLOGITEO BARPERIODOY
-+2 TOTALFOROS   FOROSPERIODO EEAPERIODOY
--2 PLIROTEO     FOROLOGITEO TOTALFOROS
-+2 TOTALKOSTOS  TAPODOXES IKAERDOTIS
->2 TOTAL2       PLIROTEO TOTALKOSTOS
-^2 TOTAL2       10 11 1
-+2 TOTAL2       PLIROTEO TOTALFOROS IKATOTAL
-d2 imeromisthio
-d0 meres
-d0 oresnyxta
-d0 meresargia
-'''
-
-exep1 = """
-# To test exception1
-? val1 val2
-"""
-
-exep2 = """
-# to test exception 2
-} SOMETHING
-"""
-
-als = """
-# Δοκιμαστικός αλγόριθμος
-%4 pososto-ika             45
-%4 pososto-ika-ergazomenoy 15
-=2 apodoxes                350
-*2 ika-ergazomenoy apodoxes pososto-ika-ergazomenoy 
-"""
-
-
-class TestCalculator(TestCase):
-    def test_calculator_good(self):
-        self.assertEqual(clc.calculator(
-            mis, {'imeromisthio': 60, 'meres': 25,
-                  'pikatotal': 44.15, 'pikaenos': 14.98}, fundic),
-        {'imeromisthio': Decimal('60.00'), 'meres': Decimal('25'),
-         'pikatotal': Decimal('44.150000'), 'pikaenos': Decimal('14.980000'),
-         'BARPERIODOY': Decimal('14'), 'PIKAT': Decimal('0.4415'),
-         'PIKAE': Decimal('0.1498'), 'PNYXTA': Decimal('0.2500'),
-         'PARGIA': Decimal('0.7500'), 'SYNTFOROY': Decimal('0.22'),
-         'POROM': Decimal('0.150000'), 'ORESANAMERA': Decimal('6.6667'),
-         'OROMISTHIO': Decimal('9.00'), 'oresnyxta': Decimal('0'),
-         'NYXTPROS': Decimal('0.00'), 'meresargia': Decimal('0'),
-         'ARGIAPROS': Decimal('0.00'), 'NAPOD': Decimal('1500.00'),
-         'TAPODOXES': Decimal('1500.00'), 'IKATOTAL': Decimal('662.25'),
-         'IKAERGNO': Decimal('224.70'), 'IKAERDOTIS': Decimal('437.55'),
-         'FOROLOGITEO': Decimal('1275.30'), 'PAIDIA': Decimal('0'),
-         'FOROSPERIODO': Decimal('144.85'), 'EEAPERIODOY': Decimal('9.20'),
-         'TOTALFOROS': Decimal('154.05'), 'PLIROTEO': Decimal('1121.25'),
-         'TOTALKOSTOS': Decimal('1937.55'), 'TOTAL2': Decimal('1937.55')})
-
-    def test_calculator_exception1(self):
-        self.assertRaises(ValueError, clc.calculator, exep1, {'val1': 15})
-
-    def test_calculator_exception2(self):
-        self.assertRaises(ValueError, clc.calculator, exep2, {'val1': 15})
-
-    def test_load_algorithm(self):
-        dat = {'IMEROMISTHIO': 60,
-               'meres': 25,
-               'POSOSTO-IKA': 44.15,
-               'POSOSTO-IKA-ERGAZOMENOY': 14.98,
-               'paidia': 3}
-        # alg = clc.load_algorithm()
-        print(clc.calc2('/home/ted/alg1.txt', dat, fundic))
-        # print(clc.calc2('/home/ted/alg1.txt', dat, fundic))
-        # print(clc.calc2('/home/ted/alg1.txt', dat, fundic))
+if __name__ == "__main__":
+    from tedutil.calculator import load_algorithm
+    from tedutil.calculator import calculate
+    from tedutil.calculator import calc
+    alg = load_algorithm('algorithm.txt')
+    dat = {'IMEROMISTHIO': 60,
+           'meres': 25,
+           'POSOSTO-IKA': 44.15,
+           'POSOSTO-IKA-ERGAZOMENOY': 14.98,
+           'paidia': 3}
+    # print('\n'.join([f'{i.operator} {i.result} {i.pars}' for i in alg]))
+    print('\n'.join('%-30s: %9s' % (i, j)
+                    for i, j in calculate(alg, dat).items()))
+    print(calc.registered())
