fb89af26e08d8de6de50fe2104cafcdd28b47cce
Ted Lazaros <tedlaz@gmail.com>
2021-05-23 13:24:28 +0300

    version 0.14.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.hexversion < 0x3040000:
 
 setup(
     name="tedutil",
-    version="0.13.0",
+    version="0.14.0",
     description="Various utility functions",
     long_description="Various utility functions",
     url="https://github.com/tedlaz/tedutil",
